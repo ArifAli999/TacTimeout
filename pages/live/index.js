a2680ff950d1465a81b36aa051ef71b5733e2676
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-04-07 14:52:48 +0300

    Testing

--- pages/live/index.js
+++ pages/live/index.js
@@ -1,20 +1,16 @@
 import Link from "next/link";
 
-export async function getServerSideProps() {
-    // Fetch data from external API
-    const res = await fetch(`https://api.pandascore.co/matches/running?sort=&page=1&per_page=50&token=a1trG0pytDA2N0RXkJVlWqA6MOb2aY8ii9szwMze-OabnW9QPu0`)
-    const data = await res.json()
-    const games = data;
+export const getStaticProps = async () => {
 
-    // Pass data to the page via props
+    const res = await fetch(`https://api.pandascore.co/matches/running?&token=a1trG0pytDA2N0RXkJVlWqA6MOb2aY8ii9szwMze-OabnW9QPu0 `);
+    const data = await res.json();
+    
+
     return {
-        props:
-        {
-
-            results: games,
+        props: {
+         game: data
         }
     }
-
 }
 
 const liveGames = ({ results }) => {
