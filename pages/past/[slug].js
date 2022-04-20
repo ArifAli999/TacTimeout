9fdcdddb3bc858d6b970988b48a118b2a0a98805
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-04-20 07:15:41 +0300

    Name fixes in past games

--- pages/past/[slug].js
+++ pages/past/[slug].js
@@ -88,7 +88,33 @@ export default function PastGames({ game, plays }) {
 
           <div className="inner-box">
             {/** Fetch team and display their corresponding score - */}
-         
+            <div className=' scoreboards'>
+              <div className='team-one'>
+                <div className='team-name'>
+
+                  {g.opponents.slice(0, -1).map((o) =>
+                    <figure className='immage-box' key={o.id}>
+                      <img src={o.opponent.image_url} className='team-img is-rounded'></img>
+                      <figcaption key={o.opponent.id}>
+                        {o.opponent.name}</figcaption> </figure>)}
+                </div>
+              </div>
+              <div className="scoresrn">
+                <span className="teamoneSc">1</span>
+                -
+                <span className="teamoneSc">3</span>
+              </div>
+              <div className='team-one'>
+                <div className='team-name'>
+
+                  {g.opponents.slice(-1).map((o) =>
+                    <figure className='immage-box' key={o.id}>
+                      <img src={o.opponent.image_url} className='team-img is-rounded' key={o.opponent.name}></img>
+                      <figcaption key={o.opponent.id}>
+                        {o.opponent.name}</figcaption> </figure>)}
+                </div>
+              </div>
+            </div>
 
             <div id="tabs-with-content">
               <div className="tabs is-toggle is-toggle-rounded is-centered">
@@ -113,7 +139,7 @@ export default function PastGames({ game, plays }) {
                       <div className="dark"><span className="is-pulled-left">Game:</span>  <span className="is-pulled-right">  {g.videogame && g.videogame.name} </span></div>
                       <div className="dark alt"><span className="is-pulled-left">Tournament</span>  <span className="is-pulled-right"> {g.tournament && g.tournament.name} | </span></div>
                       <div className="dark"><span className="is-pulled-left">Time & Date</span>  <span className="is-pulled-right"> <Moment format='DD/M - HH:MM'>{g.scheduled_at}</Moment></span></div>
-                      <div className="dark alt"><span className="is-pulled-left">Winner</span>  <span className="is-pulled-right"> {g.winner.name}  </span></div>
+                      <div className="dark alt"><span className="is-pulled-left">Winner</span>  <span className="is-pulled-right">   </span></div>
                     </div>
                   </div>
 
