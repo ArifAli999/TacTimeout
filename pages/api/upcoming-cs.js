089965c41d9c967aedcf8f510d4da10fbbf9f5f9
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-04-29 00:06:11 +0100

    Main test

--- pages/api/upcoming-cs.js
+++ pages/api/upcoming-cs.js
@@ -1,14 +1,38 @@
+import Cors from 'cors'
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+  const response = await fetch('https://api.pandascore.co/matches/upcoming?filter[videogame]=cs-go&token=a1trG0pytDA2N0RXkJVlWqA6MOb2aY8ii9szwMze-OabnW9QPu0')
+  const jsonData = await response.json()
+  res.status(200).json(jsonData)
+}
+
+
+    
+
+
+export default handler
 // fetch upcoming CS:GO games.
 
-export async function GetGames() {
-    //works.
-    const response = await fetch('https://api.pandascore.co/matches/upcoming?filter[videogame]=cs-go&token=a1trG0pytDA2N0RXkJVlWqA6MOb2aY8ii9szwMze-OabnW9QPu0')
-    const jsonData = await response.json()
-    return jsonData
-}
 
-export default async function handler(req, res) {
-    const jsonData = await getData()
-    res.status(200).json(jsonData)
-}
+
